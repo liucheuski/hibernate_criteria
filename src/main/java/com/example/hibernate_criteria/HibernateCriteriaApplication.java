59121ec1ca0facf95e82dc8cc9483a417fb651bf
liucheuski <liucheuski@mail.ru>
2022-05-03 11:17:13 +0300

    hibernate_criteria done

---
+++ src/main/java/com/example/hibernate_criteria/HibernateCriteriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_criteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCriteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateCriteriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hibernate_criteria/HibernateCriteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_criteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateCriteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
